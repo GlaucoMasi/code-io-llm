50db556aa2204c181606dc4e55b9803cff05c9da
Glauco Masi <glaucomasi@gmail.com>
2026-03-08 12:44:21 +0100

    Model outline

---
+++ src/model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class CausalSelfAttention(nn.Module):
+
+
+
+class FeedForward(nn.Module):
+
+
+
+
+class TrasformerBlock(nn.Module):
+
+
+
+
+class CodeIOLLM(nn.Module):
+
+
+
